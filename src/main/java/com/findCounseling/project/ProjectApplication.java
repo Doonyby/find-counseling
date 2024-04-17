055bcc043dc1a500f951c304c2468fad2368d486
Doonyby <doonyby@gmail.com>
2024-04-17 11:07:12 -0600

    first commit

---
+++ src/main/java/com/findCounseling/project/ProjectApplication.java
@@ -0,0 +1,27 @@
+package com.findCounseling.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+		System.out.println();
+		System.out.println("       *       ");
+		System.out.println("      ***      ");
+		System.out.println("     * * *     ");
+		System.out.println("    *  *  *    ");
+		System.out.println("   *   *   *   ");
+		System.out.println("  *    *    *  ");
+		System.out.println(" *     *     * ");
+		System.out.println("****   *   ****");
+		System.out.println("  ***********  ");
+		System.out.println("   ||     ||   ");
+		System.out.println("  (||)   (||)   ");
+		System.out.println(" ((||)) ((||))  ");
+		System.out.println("((BLAST OFFFF))");
+		System.out.println();
+	}
+}
